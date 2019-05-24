acea5e5c795ed8da3cc47928c4e8198b6137ea75
Axel Eriksson <Axel.DTK.Eriksson@lystkom.se>
2019-05-24 12:45:37 +0200

    Add project files.

---
+++ nyabanken/Properties/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Note to teacher!:
+//Mycket av koden har vi jobbat tillsammans med, därav ser det väldigt kopierat ut!
+namespace Bank
+{
+    ///class Customer
+    {
+        public string name { get; set; }
+        public int balance { get; set; }
+        public string customerInfo()
+        {
+            return this.name;
+        }
+    }
+}
